daa0753f9fe67a44fdaeca6934655563f1d0e9c5
Joseph Anderson <jandrewanderson1995@gmail.com>
2022-06-08 15:32:11 -0500

    added correct images

--- src/utils/ProjectData.js
+++ src/utils/ProjectData.js
@@ -27,12 +27,41 @@ const projectInfo = [
         description: ""
     },
     {
-        name: "Weather App",
-        href: "https://jandrewanderson.github.io/server-side-apis-weather-dashboard/",
-        img: weather,
-        alt: "Image of my weather app",
+        name: "Game Parrot",
+        href: "https://game-parrot.herokuapp.com/",
+        img: "https://raw.githubusercontent.com/game-parrot/game-parrot/develop/client/src/assets/game-parrot.herokuapp.com.png",
+        alt: "Image of Game Parrot",
         description: ""
     },
+    {
+        name: "Game Parrot Chatroom",
+        href: "https://game-parrot-chatroom.herokuapp.com/",
+        img: "https://cdn.discordapp.com/attachments/967611990094782527/984189779732213760/game-parrot-chatroom.herokuapp.com_.png",
+        alt: "Image of Game Parrot Chatroom",
+        description: ""
+    },
+    {
+        name: "S.N.A (Backend)",
+        href: "https://github.com/jandrewanderson/social-network-api",
+        img: "https://github.com/jandrewanderson/social-network-api/raw/main/assets/user-gif.gif",
+        alt: "Gif of Social Network API Project",
+        description: ""
+    },
+    {
+        name: "Tech Blog",
+        href: "https://the-tech-blog-jaanderson.herokuapp.com/",
+        img: "https://github.com/jandrewanderson/tech-blog/raw/main/assets/images/the-tech-blog.png",
+        alt: "Image of my tech blog",
+        description: ""
+    },
+    {
+        name: "T.P.G (Backend)",
+        href: "https://github.com/jandrewanderson/team-profile-generator",
+        img: "https://cdn.discordapp.com/attachments/967611990094782527/984192450501439558/10-object-oriented-programming-homework-demo.png",
+        alt: "Image of my tech blog",
+        description: ""
+    },
+    
 ]
 
 module.exports = projectInfo
